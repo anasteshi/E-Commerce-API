470d23e45795136a3675f15d1f6b742750d8bb36
Anastasiia <anasteshi13@gmail.com>
2026-07-02 22:02:16 +0200

    Set up basic routes

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ require("dotenv").config()
 // express
 const express = require("express")
 const app = express()
-
 const port = process.env.PORT || 5001
+
+// database
 const connectDB = require("./db/connect")
+
+// middleware
+app.use(express.json()) // in order to access data in req.body
+
+// routes
+app.get("/", async (req, res) => {
+    res.send("E-commerce API")
+})
 
 const start = async () => {
     try {
