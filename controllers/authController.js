feaabb7e61b1c4d5e16dfdc2732db824613c8e67
Anastasiia <anasteshi13@gmail.com>
2026-07-03 12:49:58 +0300

    Initial setup for register route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,16 @@
+const { StatusCodes } = require("http-status-codes")
+const CustomError = require("../errors")
 const User = require("../models/User")
 
 const register = async (req, res) => {
-    res.send("Register route")
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        throw new CustomError.BadRequestError("Invalid credentials")
+    }
+
+    const user = await User.create({ name, email, password })
+    res.status(StatusCodes.CREATED).json(user)
 }
 
 const login = async (req, res) => {
