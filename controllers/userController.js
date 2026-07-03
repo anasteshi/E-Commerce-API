901595fc22dbe6827ae583b0a4b379f5e886a63b
Anastasiia <anasteshi13@gmail.com>
2026-07-04 00:29:26 +0300

    Set up getAllUsers and getSingleUser controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
+const User = require("../models/User")
+const { StatusCodes } = require("http-status-codes")
+const CustomError = require("../errors")
+
 const getSingleUser = async (req, res) => {
-    res.send(req.params)
+    const userID = req.params.id
+    const user = await User.findById(userID).select("-password")
+
+    if (!user) {
+        throw new CustomError.NotFoundError(`No user with id: ${userID}`)
+    }
+    res.status(StatusCodes.OK).json({ user })
 }
 
 const getAllUsers = async (req, res) => {
-    res.send("Get all users")
+    const users = await User.find({ role: "user" }).select("-password")
+    res.status(StatusCodes.OK).json({ users })
 }
 
 const showCurrentUser = async (req, res) => {
