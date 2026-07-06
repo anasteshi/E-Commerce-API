8fb3921b005f3babc87040361435ac6ff5e13469
Anastasiia <anasteshi13@gmail.com>
2026-07-06 20:18:09 +0300

    Complete updatePassword controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,25 @@ const updateUser = async (req, res) => {
 }
 
 const updateUserPassword = async (req, res) => {
-    res.send(req.body)
+    const { oldPassword, newPassword } = req.body
+    const { userID } = req.user
+
+    if (!oldPassword || !newPassword) {
+        throw new CustomError.BadRequestError("Please provide both values")
+    }
+
+    const user = await User.findOne({ _id: userID })
+    const isPasswordCorrect = await user.comparePassword(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new CustomError.UnauthenticatedError("Invalid credentials")
+    }
+    user.password = newPassword
+    user.save()
+
+    res.status(StatusCodes.OK).json({
+        msg: "User password was successfully updated!",
+    })
 }
 
 module.exports = {
