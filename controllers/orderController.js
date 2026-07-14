07181342bde6354735bbb210a8d0a36ff986606a
Anastasiia <anasteshi13@gmail.com>
2026-07-15 02:07:35 +0300

    Set up createOrder controller and check product info

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,9 +1,45 @@
 const Order = require("../models/Order")
+const Product = require("../models/Product")
 const CustomError = require("../errors")
 const { StatusCodes } = require("http-status-codes")
 const utils = require("../utils")
 
 const createOrder = async (req, res) => {
+    const { items: cartItems, tax, shippingFee } = req.body
+    if (!cartItems || cartItems.length < 1) {
+        throw new CustomError.BadRequestError("No cart items provided")
+    }
+    if (!tax || !shippingFee) {
+        throw new CustomError.BadRequestError(
+            "Please provide tax and shipping fee",
+        )
+    }
+
+    let orderItems = []
+    let subtotal = 0
+
+    for (const item of cartItems) {
+        const productID = item.product
+        const amount = item.amount
+        const product = await Product.findById(productID)
+
+        if (!product) {
+            throw new CustomError.NotFoundError(
+                `No product with id: ${productID}`,
+            )
+        }
+
+        const { name, price, image } = product
+        orderItems = [
+            ...orderItems,
+            { name, price, image, amount, product: productID },
+        ]
+
+        subtotal += price * amount
+
+        console.log(orderItems)
+        console.log(subtotal)
+    }
     res.send("Create order")
 }
 
